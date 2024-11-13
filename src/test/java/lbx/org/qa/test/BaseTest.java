4d9ab021d43fe430b0f3d80f85351afe85c630dd
Roman Fihol <lucky.figol@gmail.com>
2024-11-13 21:40:33 +0100

    Added new method for checking age

--- src/test/java/lbx/org/qa/test/BaseTest.java
+++ src/test/java/lbx/org/qa/test/BaseTest.java
@@ -2,7 +2,6 @@ package lbx.org.qa.test;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
-import lbx.org.qa.example.google.page.GoogleSearch;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
